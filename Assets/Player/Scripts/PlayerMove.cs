6ef8023698abdb91d97ef0d93f6cd7bfd257c005
anditsoon <83330939+anditsoon@users.noreply.github.com>
2024-06-24 18:20:25 +0900

    카메라/캐릭터 회전

--- Assets/Player/Scripts/PlayerMove.cs
+++ Assets/Player/Scripts/PlayerMove.cs
@@ -11,16 +11,22 @@ public class PlayerMove : MonoBehaviour
     CharacterController cc;
 
     // 중력 변수
-    float gravity = -20f;
+    float gravity = -80f;
 
     // 수직 속력 변수
     float yVelocity = 0;
 
     // 점프력 변수
-    public float jumpPower = 10f;
+    public float jumpPower = 1f;
+
+    // 점프 상태 변수
+    public bool isJumping = false;
 
     // 점프 상태 변수
     //public bool isJumping = 
+
+    // 모양의 게임 오브젝트
+    public GameObject model;
 
     private void Start()
     {
@@ -45,11 +51,25 @@ public class PlayerMove : MonoBehaviour
         // 2-1. 메인 카메라를 기준으로 방향을 변환한다
         dir = Camera.main.transform.TransformDirection(dir);
 
-        // 2-2. 만일, 키보드 [spacebar] 를 눌렀다면,
-        if (Input.GetButtonDown("Jump"))
+        // 2-2. 만일, 점프 중이었고, 다시 바닥에 착지했다면
+        if (cc.collisionFlags == CollisionFlags.Below)
         {
-            // 캐릭터 수직 속도에 점프력을 적용한다
+            // 만일, 점프 중이었다면,
+            if (isJumping)
+            {
+                // 점프 전 상태로 초기호한다
+                isJumping = false;
+                // 캐릭터 수직 속도를 0으로 만든다
+                yVelocity = 0;
+            }
+        }
+
+        // 2-3. 만일, 키보드 [spacebar] 키를 입력했고, 점프를 하지 않은 상태라면,
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            // 캐릭터 수직 속도에 점프력을 적용하고 점프 상태로 변경한다
             yVelocity = jumpPower;
+            isJumping = true;
         }
 
         // 2-3. 캐릭터 수직 속도에 중력 값을 적용한다
@@ -59,5 +79,15 @@ public class PlayerMove : MonoBehaviour
         //3. 이동 속도에 맞춰 이동한다
         // p = p0 + vt
         cc.Move(dir * moveSpeed * Time.deltaTime);
+
+        Vector3 modelDir = dir;
+        modelDir.y = 0;
+
+        // dir 의 크기가 0보다 크면 (움직일때만)
+        if (modelDir.magnitude > 0)
+        {
+            // 움직이는 방향을 모양의 앞방향으로 설정
+            model.transform.forward = modelDir;
+        }
     }
 }
